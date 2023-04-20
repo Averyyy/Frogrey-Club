a693bc116646b3bb6d5c6dfd64937a1640e6cf62
Avery <hq443@nyu.edu>
2023-04-20 18:26:55 +0800

    change render ratio to 0.5

--- js/script-three.js
+++ js/script-three.js
@@ -6,7 +6,7 @@ let container, stats, gui, params, controls;
 let scene, camera, renderer;
 let time = 0;
 let frame = 0;
-let renderRatio = 1;
+let renderRatio = 0.5;
 
 let lights = [];
 let targetBox;
@@ -17,10 +17,10 @@ let local = false;
 let videoTexture;
 
 
-let elapsedTime = 0;
-let frameCount = 0;
+// let elapsedTime = 0;
+// let frameCount = 0;
 
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 
 function initTHREE() {
   params = {
@@ -149,20 +149,20 @@ function animate() {
   frame++;
 
   // Update elapsedTime and frameCount
-  elapsedTime += clock.getDelta();
-  frameCount++;
+  // elapsedTime += clock.getDelta();
+  // frameCount++;
 
-  // Calculate and display frame rate every second
-  if (elapsedTime >= 1) {
-    const frameRate = frameCount / elapsedTime;
-    // console.log("Frame rate:", frameRate);
-    elapsedTime = 0;
-    frameCount = 0;
-    if (frameRate < 60) {
-      renderRatio = 0.5;
-      renderer.setPixelRatio(window.devicePixelRatio * renderRatio);
-    }
-  }
+  // // Calculate and display frame rate every second
+  // if (elapsedTime >= 1) {
+  //   const frameRate = frameCount / elapsedTime;
+  //   // console.log("Frame rate:", frameRate);
+  //   elapsedTime = 0;
+  //   frameCount = 0;
+  //   if (frameRate < 60) {
+  //     renderRatio = 0.5;
+  //     renderer.setPixelRatio(window.devicePixelRatio * renderRatio);
+  //   }
+  // }
 
   updateTHREE();
   // // update the lights
